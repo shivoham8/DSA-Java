e3a092b3cb11d881e4015f35aa90cd618585f133
Ajay Gupta <ajaygupta230085@gmail.com>
2026-04-20 16:12:32 +0530

    [125] [Time Beats: 31.51%] [Memory Beats: 34.69%] - LeetPush

---
+++ Valid-Palindrome.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public boolean isPalindrome(String s) {
+3        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+4        int left = 0, right = s.length() - 1;
+5        while (left <= right) {
+6            if (s.charAt(left) != s.charAt(right)) {
+7                return false;
+8            }
+9            left++;
+10            right--;
+11        }
+12        return true;
+13    }
+14}
